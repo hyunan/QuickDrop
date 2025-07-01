317261c361b23414437b73e07154ac252899cd59
hyunan <68716940+hyunan@users.noreply.github.com>
2025-07-01 12:30:38 -0700

    updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import { VStack, Center } from '@chakra-ui/react'
 import MainModal from './components/ui/main-modal'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <VStack minH="100vh" >
+      <VStack minH="100vh" bg="gray.400">
         <Center w="full" flex="1">
           <MainModal />
         </Center>
