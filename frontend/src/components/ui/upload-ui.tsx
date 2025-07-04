6948b998b2611392784f15239a024c551d67469b
hyunan <68716940+hyunan@users.noreply.github.com>
2025-07-03 22:25:56 -0700

    profiles and scrollable send list made

--- frontend/src/components/ui/upload-ui.tsx
+++ frontend/src/components/ui/upload-ui.tsx
@@ -1,12 +1,28 @@
-import { Text } from "@chakra-ui/react"
+import { Box, Text, SimpleGrid, Button } from "@chakra-ui/react"
 
 import FileChoose from "./Upload/file-choose"
+import Profile from "./Util/profile"
 
 const UploadUI = () => {
+    const devices = Array.from({ length: 4 }, (_, i) => `Device ${i + 1}`)
+
     return (
         <>
-            <Text>UPLOADING!</Text>
-            <FileChoose onFileSelect={(file) => console.log(file.name)}></FileChoose>
+            <Text>UPLOADING SCREEN</Text>
+            <FileChoose onFileSelect={(file) => console.log(`Uploaded: ${file.name}. Total size: ${file.size} bytes.`)}></FileChoose>
+            <Text>Send to:</Text>
+            <Box
+                maxH="300px"
+                overflowY="auto"
+                w="full"
+                px={2}
+                >
+                <SimpleGrid columns={[2, 6]}>
+                    {devices.map((device, idx) => (
+                        <Profile name={device}></Profile>
+                    ))}
+                </SimpleGrid>
+            </Box>
         </>
     )
 }
