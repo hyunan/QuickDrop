2a0b8b68a9ea83a7196c659dfbbfe11d1de8a3e7
Ray An <hyunseok930@gmail.com>
2025-07-21 20:31:01 -0700

    more changes

---
+++ app/presense.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Request
+from datetime import datetime, timedelta, timezone
+from typing import Dict
+
+router = APIRouter()
+
+clients: Dict[str, datetime] = {}
+CLIENT_TIMEOUT = timedelta(seconds=10)
+
+@router.post("/api/announce_{device_name}")
+async def announce(device_name: str):
+    clients[device_name] = datetime.now(timezone.utc)
+    return {"message": "Client announced", "name" : device_name}
+
+@router.get("/api/clients")
+async def get_clients():
+    now = datetime.now(timezone.utc)
+    inactive = [ip for ip, ts in clients.items() if now - ts >= CLIENT_TIMEOUT]
+    for ip in inactive:
+        del clients[ip]
+    clients.clear()
+    return list(clients.keys())
--- frontend/src/components/ui/main-modal.tsx
+++ frontend/src/components/ui/main-modal.tsx
@@ -1,35 +1,75 @@
-import { useMemo, useState } from "react";
-import { Box, Flex, Spacer, VStack } from "@chakra-ui/react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import axios from "axios";
 
-import TopBar from "./top-bar";
-// import UploadUI from "./upload-ui";
-import ReceiveUI from "./receive-ui";
-import ViewToggle from "./Util/view-toggle";
 import { GenerateDeviceName } from "./Util/generate-device-name"
+import UploadUI from "./upload-ui";
+import SenderUI from "./sender-ui";
 
-const MainModal = () => {    
-    const [mode, setMode] = useState<"upload" | "receive">("upload")
+const MainModal = () => {
+    // Announce to server activity
+    useEffect(() => {
+        const announce = async () => {
+            try {
+                await axios.post(`http://0.0.0.0:8000/api/announce_${deviceName}`);
+                console.log("Announced presense to server");
+            } catch (e) {
+                console.error("Failed to announce presense", e);
+            }
+        }
+
+        announce();
+
+        const interval = setInterval(announce, 3500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    // Get client list
+    useEffect(() => {
+        const get_devices = async () => {
+            try {
+                let res = await axios.get("http://0.0.0.0:8000/api/clients");
+                setDevices(res.data)
+            } catch (e) {
+                console.error("Failed to announce presense", e);
+            }
+        }
+
+        get_devices();
+
+        const interval = setInterval(get_devices, 1500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const [uploadedFileName, setUploadedFileName] = useState<string>("");
+    const [devices, setDevices] = useState([])
     const deviceName = useMemo(() => GenerateDeviceName(), [])
+    console.log(`Name is ${deviceName}`)
+
+    const handleFileUpload = useCallback((filename: string) => {
+        setUploadedFileName(filename);
+        console.log(`[handleFileUpload] Uploaded file's name is: ${filename}`);
+    }, []);
+
+    useEffect(() => {
+        if (uploadedFileName) {
+            console.log(`RECEIVED NAME: ${uploadedFileName}`);
+        }
+    }, [uploadedFileName]);
 
     return (
-        <Box
-            bg="white"
-            h="550px"
-            w="700px"
-            borderRadius="12px"
-            overflow="hidden"
-        >
-            <VStack bg="blue" h="full">
-                <TopBar deviceName={deviceName} />
-                <Flex w="100%" pr="2">
-                    <Spacer />
-                    <ViewToggle mode={mode} setMode={setMode} />
-                </Flex>
-                {mode === "upload" 
-                    ? <ReceiveUI /> 
-                    : <ReceiveUI />}
-            </VStack>
-        </Box>
+        <>
+            <Flex direction={{ base: "column-reverse", md: "row" }} w="100%" h="100vh">
+                <Box flexBasis={{ base: "40%", md: "30%" }} bg={"gray.100"}>
+                    <UploadUI onFileUploaded={handleFileUpload} />
+                </Box>
+                <Box flexBasis={{ base: "60%", md: "70%" }} bg={"blue.300"}>
+                    <SenderUI deviceName={deviceName} devices={devices} />
+                </Box>
+            </Flex>
+        </>
     )
 }
 
